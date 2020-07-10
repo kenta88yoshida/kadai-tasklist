41ff3273dfd6cf60906475db7a92471c2b402732
kenta.yoshida <yosi-knt@docomo.ne.jp>
2020-07-10 08:26:01 +0000

    add column

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_09_130911) do
+ActiveRecord::Schema.define(version: 2020_07_10_081801) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "Status"
+    t.string "status"
   end
 
 end
