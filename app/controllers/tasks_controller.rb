f73462ddbf96cedf8033bca33be0ae0953ded6e3
kenta.yoshida <yosi-knt@docomo.ne.jp>
2020-07-17 15:22:33 +0000

    tasklist-re

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,7 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in, only: [:index, :show, :new, :edit]
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
+  
   def index
     @tasks = current_user.tasks.page(params[:page])
   end 
@@ -47,11 +48,16 @@ class TasksController < ApplicationController
   
   private
   
-  def set_task
-    @task = current_user.tasks.find(params[:id])
-  end 
-  
   def task_params
     params.require(:task).permit(:content, :status)
   end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to "/"
+    end
+  end
 end
+
+
